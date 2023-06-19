5b2dd29ba8cd38aa9881ae552a2d21271fe7708b
Berna <bernayasar@hotmail.co.uk>
2023-06-19 18:38:04 +0100

    added routes on app.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './components/Home';
+import { AboutUs } from './components/AboutUs';
+import { ContactUs } from './components/ContactUs';
 
 function App() {
   return (
     <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/about-us' element={<AboutUs/>} />
+      <Route path='/contact-us' element={<ContactUs/>} />
+    </Routes>
+
     </BrowserRouter>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
