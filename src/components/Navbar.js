025d3cdc73c38934aa4de121a9134c99f6b6fae0
Berna <bernayasar@hotmail.co.uk>
2023-06-19 19:56:31 +0100

    moved img locations, added img to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom"
+import companyLogo from '../resources/Logo.svg'
 
 export const Navbar = () => {
     return (
         <nav>
+            <img src={companyLogo} alt="Company Logo" />
             <Link to='/'>Home</Link>
             <Link to='/about-us'>About Us</Link>
             <Link to='/contact-us'>Contact Us</Link>
